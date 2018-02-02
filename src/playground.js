7f4ce6ed234248d5fcc6ddb9963eca4e173bfc62
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-02-01 18:49:45 -0800

    pull out playground sample into standalone file

--- src/playground.js
+++ src/playground.js
@@ -1,6 +1,5 @@
 import Maze from './maze';
-import tiles from '../assets/tiles.png';
-import avatar from '../assets/avatar.png';
+import sampleConfig from './sampleConfig';
 
 const container = document.createElement('div');
 container.id = 'container';
@@ -8,104 +7,7 @@ document.body.appendChild(container);
 
 const maze = window.maze = new Maze()
 
-maze.init({
-  skinId: 'farmer',
-  skin: {
-    pegmanHeight: 52,
-    pegmanWidth: 49,
-    pegmanYOffset: -8,
-    tiles,
-    avatar
-  },
-  level: {
-    startDirection: 0,
-    serializedMaze: [
-      [
-        { tileType: 0 },
-        { tileType: 0 },
-        { tileType: 0 },
-        { tileType: 0 },
-        { tileType: 0 },
-        { tileType: 0 },
-        { tileType: 0 },
-        { tileType: 0 },
-      ],
-      [
-        { tileType: 0 },
-        { tileType: 0 },
-        { tileType: 0 },
-        { tileType: 0 },
-        { tileType: 0 },
-        { tileType: 0 },
-        { tileType: 0 },
-        { tileType: 0 },
-      ],
-      [
-        { tileType: 0 },
-        { tileType: 1 },
-        { tileType: 1, value: 1, range: 1 },
-        { tileType: 1 },
-        { tileType: 1, value: 2, range: 2 },
-        { tileType: 1 },
-        { tileType: 0 },
-        { tileType: 0 },
-      ],
-      [
-        { tileType: 0 },
-        { tileType: 1, value: 4, range: 4 },
-        { tileType: 0 },
-        { tileType: 1 },
-        { tileType: 0 },
-        { tileType: 1, value: 3, range: 3 },
-        { tileType: 0 },
-        { tileType: 0 },
-      ],
-      [
-        { tileType: 0 },
-        { tileType: 1 },
-        { tileType: 1 },
-        { tileType: 2 },
-        { tileType: 1 },
-        { tileType: 1 },
-        { tileType: 0 },
-        { tileType: 0 },
-      ],
-      [
-        { tileType: 0 },
-        { tileType: 1, value: 3, range: 3 },
-        { tileType: 0 },
-        { tileType: 1 },
-        { tileType: 0 },
-        { tileType: 1, value: 1, range: 1 },
-        { tileType: 0 },
-        { tileType: 0 },
-      ],
-      [
-        { tileType: 0 },
-        { tileType: 1 },
-        { tileType: 1, value: 2, range: 2 },
-        { tileType: 1 },
-        { tileType: 1, value: 4, range: 4 },
-        { tileType: 1 },
-        { tileType: 0 },
-        { tileType: 0 },
-      ],
-      [
-        { tileType: 0 },
-        { tileType: 0 },
-        { tileType: 0 },
-        { tileType: 0 },
-        { tileType: 0 },
-        { tileType: 0 },
-        { tileType: 0 },
-        { tileType: 0 },
-      ],
-    ],
-  },
-  studioApp: function() {
-    return {};
-  },
-});
+maze.init(sampleConfig);
 
 maze.render('container');
 maze.animatedMove(0, 1000);
---
+++ src/sampleConfig.js
@@ -0,0 +1,101 @@
+import tiles from '../assets/tiles.png';
+import avatar from '../assets/avatar.png';
+
+export default {
+  skinId: 'farmer',
+  skin: {
+    pegmanHeight: 52,
+    pegmanWidth: 49,
+    pegmanYOffset: -8,
+    tiles,
+    avatar
+  },
+  level: {
+    startDirection: 0,
+    serializedMaze: [
+      [
+        { tileType: 0 },
+        { tileType: 0 },
+        { tileType: 0 },
+        { tileType: 0 },
+        { tileType: 0 },
+        { tileType: 0 },
+        { tileType: 0 },
+        { tileType: 0 },
+      ],
+      [
+        { tileType: 0 },
+        { tileType: 0 },
+        { tileType: 0 },
+        { tileType: 0 },
+        { tileType: 0 },
+        { tileType: 0 },
+        { tileType: 0 },
+        { tileType: 0 },
+      ],
+      [
+        { tileType: 0 },
+        { tileType: 1 },
+        { tileType: 1, value: 1, range: 1 },
+        { tileType: 1 },
+        { tileType: 1, value: 2, range: 2 },
+        { tileType: 1 },
+        { tileType: 0 },
+        { tileType: 0 },
+      ],
+      [
+        { tileType: 0 },
+        { tileType: 1, value: 4, range: 4 },
+        { tileType: 0 },
+        { tileType: 1 },
+        { tileType: 0 },
+        { tileType: 1, value: 3, range: 3 },
+        { tileType: 0 },
+        { tileType: 0 },
+      ],
+      [
+        { tileType: 0 },
+        { tileType: 1 },
+        { tileType: 1 },
+        { tileType: 2 },
+        { tileType: 1 },
+        { tileType: 1 },
+        { tileType: 0 },
+        { tileType: 0 },
+      ],
+      [
+        { tileType: 0 },
+        { tileType: 1, value: 3, range: 3 },
+        { tileType: 0 },
+        { tileType: 1 },
+        { tileType: 0 },
+        { tileType: 1, value: 1, range: 1 },
+        { tileType: 0 },
+        { tileType: 0 },
+      ],
+      [
+        { tileType: 0 },
+        { tileType: 1 },
+        { tileType: 1, value: 2, range: 2 },
+        { tileType: 1 },
+        { tileType: 1, value: 4, range: 4 },
+        { tileType: 1 },
+        { tileType: 0 },
+        { tileType: 0 },
+      ],
+      [
+        { tileType: 0 },
+        { tileType: 0 },
+        { tileType: 0 },
+        { tileType: 0 },
+        { tileType: 0 },
+        { tileType: 0 },
+        { tileType: 0 },
+        { tileType: 0 },
+      ],
+    ],
+  },
+  studioApp: function() {
+    return {};
+  },
+}
