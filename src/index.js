abcbdbb5c332cfee858407e7bab14d778238e99b
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-05-08 14:31:54 -0700

    add single-point-of-entry index file for external requirement

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const MazeController = require('./mazeController');
+const cells = require('./cells');
+const drawMap = require('./drawMap');
+const mazeMap = require('./mazeMap');
+const subtypes = require('./subtypes');
+const tiles = require('./tiles');
+const utils = require('./utils');
+
+module.exports = {
+  MazeController,
+  cells,
+  drawMap,
+  mazeMap,
+  subtypes,
+  tiles,
+  utils,
+};
