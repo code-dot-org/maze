58a9b24ff3a027aaa519428faab22382ba0c02fb
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-05-08 16:12:15 -0700

    capitalize MazeMap, since it's also a class

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 const MazeController = require('./mazeController');
+const MazeMap = require('./mazeMap');
 const cells = require('./cells');
 const drawMap = require('./drawMap');
-const mazeMap = require('./mazeMap');
 const subtypes = require('./subtypes');
 const tiles = require('./tiles');
 const utils = require('./utils');
 
 module.exports = {
   MazeController,
+  MazeMap,
   cells,
   drawMap,
-  mazeMap,
   subtypes,
   tiles,
   utils,
