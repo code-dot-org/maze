5eeab4c947c44bbd4b5e3e329e9572427763cb11
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-05-08 17:24:18 -0700

    slim down webpack.config, since webpack 4 provides so many defaults

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,11 @@
-const path = require("path");
-const name = "maze";
-
 module.exports = {
-  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "lib"),
-    filename: name + ".js",
-    library: name,
     libraryTarget: 'umd',
-    umdNamedDefine: true
   },
-  target: 'node',
   module: {
     rules: [{
       test: /\.js$/,
       loader: "babel-loader",
     }]
   },
-  resolve: {
-    extensions: [".js"],
-  },
-  stats: {
-    colors: true
-  },
-  devtool: 'source-map',
-  devServer: { inline: true }
 };
