bcb763db095bdf8756b286c6eba415a4205458b9
Hannah Bergam <hannah.bergam@code.org>
2021-05-21 23:58:30 +0000

    progress on drawer tests

--- test/unit/neighborhoodDrawer.test.js
+++ test/unit/neighborhoodDrawer.test.js
@@ -1,10 +1,23 @@
-import {quarterCircle, cutout} from '../../src/neighborhoodDrawer';
+import {quarterCircle, cutout, makeGrid, svgElement} from '../../src/neighborhoodDrawer';
 
 describe("Painting", function () {
-    it("can give us a quarter circle and a cutout", function () {
-      pieSlice = quarterCircle(100);
-      expect(pieSlice).toEqual(`m50 50h-50c0-25 25-50 50-50z`);
-      pieCutout = cutout(100);
-      expect(cutout).toEqual(`m0 0v50c0-25 25-50 50-50z`);
-    })
+  SQUARE_SIZE = 100;
+  svg = document.getElementById('svgMaze');
+  pieSlice = quarterCircle(SQUARE_SIZE);
+  pieCutout = cutout(SQUARE_SIZE);
+
+  it("can give us a quarter circle and a cutout", function () {
+    expect(pieSlice).toEqual(`m50 50h-50c0-25 25-50 50-50z`);
+    expect(cutout).toEqual(`m0 0v50c0-25 25-50 50-50z`);
+  })
+
+  it("can make an svg element", function () {
+    newGrid = makeGrid(0, 0, svg);
+    element = svgElement(
+        "path",
+        {d: pieCutout, transform: "rotate(90)", fill: "blue"},
+        newGrid
+        );
+    expect(element).toEqual();
+  })
 });
