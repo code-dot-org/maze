38cff593b5cebbbbad75fd3af14d45f4f039182d
Hannah Bergam <hannah.bergam@code.org>
2021-05-27 19:13:02 +0000

    added comments for paint glomming, documentation for helper, moved painter to top of image

--- src/neighborhoodDrawer.js
+++ src/neighborhoodDrawer.js
@@ -9,7 +9,19 @@ const ROTATE0 = "rotate(0)";
 const CUT = "cut";
 const PIE = "pie";
 
-// Helper for creating SVG elements
+/**
+ * This is a helper for creating SVG Elements. 
+ * Groups are created by grid tile, under which paths are nested. These groups
+ * begin with "g" in the id. By checking for this when determining its position
+ * within the hierarchy, we can nest these groups just before the pegman,
+ * insuring the pegman will appear on top of the paint.
+ * 
+ * @param tag representing the element type, 'g' for group, 'path' for paths
+ * @param props representing the details of the element
+ * @param parent the parent it should be nested under
+ * @param id the unique identifier, beginning with 'g' if a group element
+ * @returns the element itself
+ */
 function svgElement(tag, props, parent, id) {
   var node = document.getElementById(id);
   if (!node) {
@@ -19,7 +31,11 @@ function svgElement(tag, props, parent, id) {
   Object.keys(props).map(function (key) {
     node.setAttribute(key, props[key])
   });
-  if (parent) {
+  if (parent && id.startsWith("g")) {
+    let pegmanElement = parent.getElementsByClassName('pegman-location')[0];
+    parent.insertBefore(node, pegmanElement);
+  }
+  else if (parent) {
     parent.appendChild(node);
   }
   return node;
@@ -46,6 +62,7 @@ function cutout(size) {
   return `m0 0v${halfSize}c0-${quarterSize} ${quarterSize}-${halfSize} ${halfSize}-${halfSize}z`
 }
 
+// For creating the groups for each grid location
 function makeGrid(row, col, svg) {
   let id = "g" + row + "." + col;
   return svgElement("g", {
@@ -54,6 +71,11 @@ function makeGrid(row, col, svg) {
   }, svg, id);
 }
 
+/**
+ * This drawer hosts all paint glomming logic. As a rule: paint glomming will
+ * work as expected as long as old colors are erased/scraped first. Otherwise,
+ * new colors will layer on top of old paint.
+ */
 module.exports = class NeighborhoodDrawer extends Drawer {
 
   constructor(map, asset, svg, squareSize, neighborhood) {
@@ -79,7 +101,7 @@ module.exports = class NeighborhoodDrawer extends Drawer {
 
   /**
    * @override
-  */
+   */
   getAsset(prefix, row, col) {
     const cell = this.neighborhood.getCell(row, col);
     // If the tile has an asset id, return the sprite asset. Ignore the asset id
